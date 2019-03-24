7fad6f1cd955c949d034966334699bff6a98ab38
Sergey Ivanovsky <43710015+serezhishe@users.noreply.github.com>
2019-03-24 15:42:30 +0300

    refactored code

--- src/index.js
+++ src/index.js
@@ -1,51 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
-let _head;
-let bool = 1;
+let stack = [];
 
-class Stack {
-  constructor(value, prev) {
-   this.prev = prev || null;
-   this.value = value || null;
-   _head = this;
-  }
-  
-
-   pop() {
-   if (_head.prev) {
-    _head = _head.prev;
-  } else {
-  if (_head.value !== null) {
-  _head.value = null;
-  } else {bool = 0;}
-    }
-  }
-   
-  
-  push(value) {
-    _head = new Stack(value, _head);
-    }
-
-}
-   
-let arr = str.split('');
-let stack = new Stack(arr[0]);
-
-for (let i = 1; i < arr.length; i++) {
+for (let i = 0; i < str.length; i++) {
   let j = 0;
-  while (bracketsConfig[j].indexOf(arr[i]) == -1) j++;
-  if (bracketsConfig[j][1] === bracketsConfig[j][0] && _head.value === null) {
-    stack.push(arr[i]);
+  while (bracketsConfig[j].indexOf(str[i]) == -1) j++;
+  if (bracketsConfig[j][1] === bracketsConfig[j][0] && stack.length == 0) {
+    stack.push(str[i]);
   } else {
-    if (arr[i] === bracketsConfig[j][1] && (_head.value === bracketsConfig[j][0] || _head.value === null)) {stack.pop();} else 
+    if (str[i] === bracketsConfig[j][1] && !stack.length) return false
+    if (str[i] === bracketsConfig[j][1] && (stack[stack.length - 1] === bracketsConfig[j][0])) {
+      stack.pop();
+    } else 
      { 
-       if (arr[i] === bracketsConfig[j][0]) stack.push(arr[i]); 
+       if (str[i] === bracketsConfig[j][0]) stack.push(str[i]);
     }
   }
 }
-
-
-if (_head.value === null && bool) {return true} else
-  {
-    return false;
-  }
+return !stack.length
 };
+
